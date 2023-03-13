e0b005e2a10d9e4abfcc172510494ff595d8082e
AndrewTrieu <andrewtrieu.2003@gmail.com>
2023-03-13 16:10:01 +0200

    Upload 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,25 @@
 import { useState } from "react";
 const Statistics = (props) => {
-  return (
-    <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p> all {props.good + props.neutral + props.bad}</p>
-      <p>
-        average{" "}
-        {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
-      </p>
-      <p>
-        positive {(props.good / (props.good + props.neutral + props.bad)) * 100}{" "}
-        %
-      </p>
-    </div>
-  );
+  if (props.good + props.neutral + props.bad === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <div>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p> all {props.good + props.neutral + props.bad}</p>
+        <p>
+          average{" "}
+          {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
+        </p>
+        <p>
+          positive{" "}
+          {(props.good / (props.good + props.neutral + props.bad)) * 100} %
+        </p>
+      </div>
+    );
+  }
 };
 const App = () => {
   // save clicks of each button to its own state
