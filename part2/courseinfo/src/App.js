d5bec9f8105f97603ea5fecff3381695af4ce334
AndrewTrieu <andrewtrieu.2003@gmail.com>
2023-03-16 17:57:41 +0200

    Upload 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,39 +1,5 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <b>total of {total} exercises</b>;
-};
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
+import React from "react";
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
