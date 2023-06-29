5563e819fb4fa08a47452167a8a71b83dccaa0c8
Andrew Trieu <andrew.trieu@kempower.com>
2023-06-29 09:46:23 +0300

    Add link to anecdote

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -36,7 +36,9 @@ const Anecdote = ({ anecdote }) => (
       {anecdote.content} by {anecdote.author}
     </h2>
     <p>has {anecdote.votes} votes</p>
-    <p>for more info see{anecdote.info}</p>
+    <p>
+      for more info see <a href={anecdote.info}>{anecdote.info}</a>
+    </p>
   </div>
 );
 
